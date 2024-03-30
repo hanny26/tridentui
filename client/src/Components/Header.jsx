76958a59e8c2e0c37facfced79c1f6e2f71a018f
hanny26 <hannyvyas109@gmail.com>
2024-03-30 20:24:11 +0530

    Add new-folder with files

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <nav className='container'>
+    <div className="logo-wrapper">
+        <img src="" alt="" />
+    </div>
+
+    <div className="nav-menu-wrapper">
+        
+    </div>
+
+   </nav>
+  )
+}
+
+export default Header
