51188fa513a79e545058bc3fcfdba01bee334eb1
hanny26 <hannyvyas109@gmail.com>
2024-03-31 21:17:32 +0530

    Cart icon

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -7,36 +7,46 @@ export const Header = () => {
     <nav id="header" className="bg-black text-white">
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div className="logo-wrapper pl-4 flex items-center">
-          <Link to="/" className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
+          <Link
+            to="/"
+            className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+          >
             <img src="" alt="logo" />
           </Link>
         </div>
         <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
-          <Link to="/" className="text-xl">Home</Link>
-          <Link to="/menu" className="text-xl">Menu</Link>
+          <Link to="/" className="text-xl">
+            Home
+          </Link>
+          <Link to="/menu" className="text-xl">
+            Menu
+          </Link>
         </div>
         <div className="flex items-center justify-center space-x-4">
           <Link to="/cart" className="text-xl">
-          <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-shopping-cart text-xl text-white"
-              >
-                <circle cx="9" cy="21" r="1" />
-                <circle cx="20" cy="21" r="1" />
-                <path d="M21 15s-2-4-2-7h-1" />
-                <path d="M17 3h-4l-1 9H7" />
-              </svg>
-          </Link >
-          <Link to="/login" className="text-xl">Log In</Link >
-          <Link to="/register" className="text-xl">Sign Up</Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-shopping-cart"
+            >
+              <circle cx="9" cy="21" r="1"></circle>
+              <circle cx="20" cy="21" r="1"></circle>
+              <path d="M1 1h4l2.68 11.413a2 2 0 0 0 1.902 1.446h8.84a2 2 0 0 0 1.902-1.446L23 6H6"></path>
+            </svg>
+          </Link>
+          <Link to="/login" className="text-xl">
+            Log In
+          </Link>
+          <Link to="/register" className="text-xl">
+            Sign Up
+          </Link>
         </div>
       </div>
     </nav>
