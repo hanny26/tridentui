dcd1fea5ab67f2fbd0401a26fd568deaaf6bfdff
hanny26 <hannyvyas109@gmail.com>
2024-04-02 20:16:09 +0530

    database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');  
 const cors = require('cors');
 
+const connectDB = require('./db/db');
+
+const app = express();
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  });
